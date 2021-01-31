87bec66ad82880e7d033c4080cde6345457eabf3
David Barrett <dlbarret@usc.edu>
2021-01-30 19:44:03 -0800

    Double Grapple Controls Update
    Splitting into two grapple hooks with customizable controls

--- Assets/Scripts/Tutorial_GrapplingGun.cs
+++ Assets/Scripts/Tutorial_GrapplingGun.cs
@@ -5,6 +5,9 @@ public class Tutorial_GrapplingGun : MonoBehaviour
     [Header("Scripts Ref:")]
     public Tutorial_GrapplingRope grappleRope;
 
+    [Header("Controls:")]
+    public KeyCode mouseButton;
+
     [Header("Layers Settings:")]
     [SerializeField] private bool grappleToAll = false;
     [SerializeField] private LayerMask IgnoreMe;
@@ -28,7 +31,7 @@ public class Tutorial_GrapplingGun : MonoBehaviour
 
     [Header("Distance:")]
     [SerializeField] private bool hasMaxDistance = false;
-    [SerializeField] private float maxDistnace = 20;
+    [SerializeField] private float maxDistance = 20;
 
     private enum LaunchType
     {
@@ -58,11 +61,11 @@ public class Tutorial_GrapplingGun : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(mouseButton))
         {
             SetGrapplePoint();
         }
-        else if (Input.GetKey(KeyCode.Mouse0))
+        else if (Input.GetKey(mouseButton))
         {
             if (grappleRope.enabled)
             {
@@ -78,13 +81,13 @@ public class Tutorial_GrapplingGun : MonoBehaviour
             {
                 if (launchType == LaunchType.Transform_Launch)
                 {
-                    Vector2 firePointDistnace = firePoint.position - gunHolder.localPosition;
-                    Vector2 targetPos = grapplePoint - firePointDistnace;
+                    Vector2 firePointDistance = firePoint.position - gunHolder.localPosition;
+                    Vector2 targetPos = grapplePoint - firePointDistance;
                     gunHolder.position = Vector2.Lerp(gunHolder.position, targetPos, Time.deltaTime * launchSpeed);
                 }
             }
         }
-        else if (Input.GetKeyUp(KeyCode.Mouse0))
+        else if (Input.GetKeyUp(mouseButton))
         {
             grappleRope.enabled = false;
             m_springJoint2D.enabled = false;
@@ -120,7 +123,7 @@ public class Tutorial_GrapplingGun : MonoBehaviour
             RaycastHit2D _hit = Physics2D.Raycast(firePoint.position, distanceVector.normalized);
             if (_hit.transform.gameObject.layer == grappableLayerNumber || grappleToAll)
             {
-                if (Vector2.Distance(_hit.point, firePoint.position) <= maxDistnace || !hasMaxDistance)
+                if (Vector2.Distance(_hit.point, firePoint.position) <= maxDistance || !hasMaxDistance)
                 {
                     grapplePoint = _hit.point;
                     grappleDistanceVector = grapplePoint - (Vector2)gunPivot.position;
@@ -175,7 +178,7 @@ public class Tutorial_GrapplingGun : MonoBehaviour
         if (firePoint != null && hasMaxDistance)
         {
             Gizmos.color = Color.green;
-            Gizmos.DrawWireSphere(firePoint.position, maxDistnace);
+            Gizmos.DrawWireSphere(firePoint.position, maxDistance);
         }
     }
 
